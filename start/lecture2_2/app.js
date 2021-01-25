92d530fbb937aa758faa54f323bfd7e4ac37b4c9
Loukit Khemka <loukitkhemka@gmail.com>
2021-01-25 10:31:16 +0530

    Added /EarthMoonDemo link

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -1,6 +1,6 @@
 import * as THREE from '../../libs/three/three.module.js';
 import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
-import { timeStamp } from 'console';
+
 
 class App{
 	constructor(){
@@ -13,6 +13,13 @@ class App{
 		this.scene = new THREE.Scene();
 		this.scene.background = new THREE.Color(0xaaaaaa);
 
+		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbffffff, 0.3);
+		this.scene.add(ambient);
+
+		const light = new THREE.DirectionalLight();
+		light.position.set(0.2,1,1);
+		this.scene.add(light);
+
 		this.renderer = new THREE.WebGLRenderer({antialias:true});
 		this.renderer.setPixelRatio(window.devicePixelRatio);
 		this.renderer.setSize(window.innerWidth, window.innerHeight);
@@ -20,15 +27,27 @@ class App{
 
 		this.renderer.setAnimationLoop(this.render.bind(this));
 		
+		const geometry = new THREE.BoxBufferGeometry();
+		const material = new THREE.MeshStandardMaterial({color:0xff0000});
+
+		this.mesh = new THREE.Mesh(geometry, material);
+
+		this.scene.add(this.mesh);
+
+		const controls = new OrbitControls(this.camera, this.renderer.domElement);
 
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
     resize(){
+		this.camera.aspect = window.innerWidth/window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
         
     }
     
 	render( ) {  
+		this.mesh.rotateY(0.01);
         this.renderer.render(this.scene, this.camera);
     }
 }
